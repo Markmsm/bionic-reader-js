545bdbfc55c4ccc90b52d7dcaf635b7ca1d4aaba
Mark <marksmunari@gmail.com>
2023-10-20 08:31:32 -0300

    [WIP] redoing project

--- README.md
+++ README.md
@@ -5,20 +5,31 @@ Criar uma aplicação X que recebe um texto e retorna esse texto com partes das 
 
 A aplicação deve:
 
-V - Ter um nome decente (criativo)
-  - Estar hospedada no github
+ - Ter um nome decente (criativo)
+ - Estar hospedada no github
 V - Ler a partir do stdin.
-        Exemplo: echo "meu texto aqui" | app
+    Exemplo: echo "meu texto aqui" | app
 V - Ler a partir de um arquivo
-        Exemplo: app meu_arquivo.txt
+    Exemplo: app meu_arquivo.txt
 V - Deixar em negrito os primeiros 50% de cada palavra (no mínimo 50%)
 V - Retornar texto no stdout
-V  - Ter um parâmetro para especificar um arquivo de saída
-        Exemplo: app -o novo_arquivo.txt meu_texto.txt
-  - Ter um parâmetro para especificar a fixação (% mínimo de negrito por palavra)
-        Exemplo: app -f 70 meu_texto.txt
-  - Ter um parâmetro para especificar os pulos (a cada quantas palavras tem um negrito)
-        Exemplo: app -j 2 meu_texto.txt
+V - Ter um parâmetro para especificar um arquivo de saída
+    Exemplo: app -o novo_arquivo.txt meu_texto.txt
+V - Ter um parâmetro para especificar a fixação (% mínimo de negrito por palavra)
+    Exemplo: app -f 70 meu_texto.txt
+ - Ter um parâmetro para especificar os pulos (a cada quantas palavras tem um negrito)
+    Exemplo: app -j 2 meu_texto.txt
+
+
+
+
+
+- Validar melhores maneiras de dividir o texto e juntar novamente
+- Validar com texto com quebra de linha (\n)
+- Validar texto com espaçamento (\t)
+- Validar início de linha ao salvar no arquivo e ler do terminal
+
+- Pode passar vários parâmetros em uma mesma chamada
 
 
 
@@ -33,3 +44,12 @@ Como rodar o app:
         $ node app -s {{FILE_TO_SAVE_PATH}} "meu texto"
     Passando um arquivo para leitura e um para escrita:
         $ node app -s {{FILE_TO_SAVE_PATH}} {{FILE_TO_READ_PATH}}
+
+
+
+palavra = 100%
+x       = 50%
+
+100x = 50 * wordlength
+
+x = (50 * wordlength) / 100
--- app.js
+++ app.js
@@ -1,53 +1,58 @@
-import * as fs from 'fs';
+import * as fs from 'fs'
 
 
-const turnHalfInitWordBold = word => {
-    const wordLength = word.length
-    const untilBold = (wordLength % 2 === 0) ? (wordLength / 2) : ((wordLength / 2) + 1)
-    const boldLetters = word.slice(0, untilBold)
-    const normalLetters = word.slice(untilBold)
+const filesPathsFromParameters = []
+const actionsParameters = []
 
-    return `<b>${boldLetters}</b>${normalLetters}`
-}
-
-const formatTextWordsToHalfBold = text => {
-    return text.split(' ')
-        .map(w => {
-            return turnHalfInitWordBold(w)
-        })
-        .join(' ')
-}
-
-const getTextFromFile = filePath => {
-    try {
-        return fs.readFileSync(filePath, 'utf-8')
-    } catch (err) {
-        throw new Error(`Error while reading file ${filePath}: `, err)
+process.argv.forEach(arg => {
+    if (arg.length === 2) {
+        actionsParameters.push(arg)
+    } else if (arg.endsWith('.txt')) {
+        filesPathsFromParameters.push(arg)
     }
-}
+})
 
-const shouldSaveInFile = commandLineArgument => {
-    return commandLineArgument === '-s'
-}
+console.log('filesPathsFromParameters = ', filesPathsFromParameters)
+console.log('actionsParameters = ', actionsParameters)
 
-const main = () => {
-    try {
-        const text = (process.argv[process.argv.length - 1].endsWith('.txt')) ?
-            getTextFromFile(process.argv[process.argv.length - 1]) :
-            process.argv[process.argv.length - 1]
-
-        const formattedText = formatTextWordsToHalfBold(text)
-
-        if(shouldSaveInFile(process.argv[2])) {
-            fs.appendFileSync(process.argv[3], `${formattedText}`)
+const formatText = text => {
+    if (actionsParameters.includes('-f')) {
+        const percentage = actionsParameters[actionsParameters.indexOf('-f') + 1]
+        if (!percentage) {
+            process.stderr.write('Error: you should pass a percentage between 50 an 100 when pass "-f" parameter.\n')
+        } else if (percentage >= 50 && percentage <= 100) {
+            return text.split(' ')
+            .map(word => {
+                const wordLength = word.length
+                const boldLength = (wordLength * percentage) / 100
+                
+                return `<b>${word.slice(0, boldLength)}</b>${word.slice(boldLength)}`
+            })
+            .join(' ') 
         }
-
-        console.log(formattedText)
-    } catch (err) {
-        console.log('Error while formating text: ', err)
     }
 }
 
-main()
-
-export { turnHalfInitWordBold, formatTextWordsToHalfBold }
+if (actionsParameters.includes('-o')) {
+    if (filesPathsFromParameters.length === 2) {
+        const fileContent = fs.readFileSync(filesPathsFromParameters[1], 'utf-8')
+        fs.appendFileSync(filesPathsFromParameters[0], formatText(fileContent))
+    } else if (filesPathsFromParameters.length === 1) {
+        process.stdin.on('data', data => {
+            fs.appendFileSync(filesPathsFromParameters[0], formatText(data.toString()))
+        })
+    } else {
+        process.stderr.write('Error: you should pass a file".txt" to save when pass "-o" parameter.\n')
+    }
+} else {
+    if (filesPathsFromParameters.length === 1) {
+        const formattedText = formatText(fs.readFileSync(filesPathsFromParameters[0], 'utf-8'))
+        process.stdout.write(`${formattedText}\n`)
+    } else if (filesPathsFromParameters.length === 0) {
+        process.stdin.on('data', data => {
+            process.stdout.write(formatText(data.toString()))
+        })
+    } else {
+        process.stderr.write('Error: you should pass just one file if no save in file parameter (-o).\n')
+    }
+}
