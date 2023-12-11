84554b02c5ba3d114dcfd25fa489f9205c05c20f
Mark <marksmunari@gmail.com>
2023-12-11 08:34:24 -0300

    WIP dealing with POST

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import * as http from 'http'
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-Type': 'application/json' })
-    res.end(JSON.stringify({ data: 'Hello World!' }))
+http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        req.on('data', (data) => {
+            const body = data.toString()
+            console.log('body = ', body)
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({
+                data: `Hello POST!`,
+                body: body
+            }))
+        })
+    } else if (req.method === 'GET') {
+        res.writeHead(200, { 'content-Type': 'application/json' })
+        res.end(JSON.stringify({ data: 'Hello World!' }))
+    }
 }).listen(8080)
 
 console.log('Server is listening on port 8080\nAccess http://localhost:8080')
