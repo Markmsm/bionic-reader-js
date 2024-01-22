18d3ce461ea9c32295d1fefc712096ace1abc5d3
Mark <marksmunari@gmail.com>
2024-01-22 08:28:13 -0300

    make app use core.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import boldText from './core.js'
 
 
 const acceptedParameters = ['-o', '-f', '-j']
@@ -47,69 +48,27 @@ for (let i = 2; i < parameters.length; i++) {
 // Format then write text
 const processText = text => {
     // Format text
-    const percentageToBold = actionParameters.get('-f') || 50
+    const percentageOfWordToBold = actionParameters.get('-f') || 50
     const wordsToSkip = Number(actionParameters.get('-j') || 0)
-    const splittedText = text.split(' ')
+    const fileType = actionParameters.get('-o') ? actionParameters.get('-o').split('.').pop() : undefined
 
-    const boldWord = (word, prefix = '', suffix = '') => {
-        const regexForPunctuationAtEnd = /[^\w]$/
-
-        if (word.startsWith('...')) {
-            prefix += '...'
-            return boldWord(word.slice(3), prefix)
-        }
-        if (word.startsWith('(') || word.startsWith('[') || word.startsWith('{')) {
-            prefix += word.slice(0, 1)
-            return boldWord(word.slice(1), prefix)
-        }
-        if (word.endsWith('...')) {
-            suffix = `...${suffix}`
-            return boldWord(word.slice(0, word.length - 3), prefix, suffix)
-        }
-        if (word.endsWith(')') || word.endsWith(']') || word.endsWith('}')) {
-            suffix = `${word.slice(word.length - 1)}${suffix}`
-            return boldWord(word.slice(0, word.length - 1), prefix, suffix)
-        }
-        if (regexForPunctuationAtEnd.test(word)) {
-            suffix = `${word.slice(word.length - 1)}${suffix}`
-            word = word.slice(0, word.length - 1)
-        }
-
-        const boldLength = Math.round((word.length * percentageToBold) / 100)
-        const partOfWordToBold = word.slice(0, boldLength)
-        const partOfWordToNotBold = word.slice(boldLength)
-        
-        const boldedWord = actionParameters.has('-o') ?
-            `<b>${partOfWordToBold}</b>${partOfWordToNotBold}` :
-            `\x1b[1m${partOfWordToBold}\x1b[0m${partOfWordToNotBold}`
-
-        return `${prefix}${boldedWord}${suffix}`
+    const processOptions = {
+        textToProcess: text,
+        percentageOfWordToBold,
+        wordsToSkip,
+        fileType
     }
-
-    for (let i = 0; i < splittedText.length; i += (wordsToSkip + 1)) {
-        const word = splittedText[i]
-
-        if (word.includes('\n')) {
-            splittedText[i] = word
-                .split('\n')
-                .map(w => (w === '' || w === '...') ? w : boldWord(w))
-                .join('\n')
-        } else {
-            splittedText[i] = (word === '' || word === '...') ? word : boldWord(word)
-        }
-    }
-
-    const formattedText = splittedText.join(' ')
+    const boldedText = boldText(processOptions)
 
     // Write text
     if (actionParameters.has('-o')) {
         try {
-            fs.appendFileSync(actionParameters.get('-o'), formattedText)
+            fs.appendFileSync(actionParameters.get('-o'), boldedText)
         } catch (err) {
             logError('Error writing in file', err)
         }
     } else {
-        process.stdout.write(`${formattedText}`)
+        process.stdout.write(`${boldedText}`)
     }
 }
 
