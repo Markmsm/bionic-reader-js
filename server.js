c7ec0b71af92f9730cc00b9823ff3e92cd619230
Mark <marksmunari@gmail.com>
2024-01-29 08:54:31 -0300

    log request

--- server.js
+++ server.js
@@ -3,23 +3,49 @@ import processText from './core.js'
 
 
 http.createServer((req, res) => {
-    if (req.method === 'POST') {
+    const httpMethod = req.method
+
+    if (httpMethod === 'POST') {
         req.on('data', (data) => {
             const body = JSON.parse(data)
             const textToProcess = body.text
             const fileType = body.fileType
             const percentageOfWordToBold = body.percentageOfWordToBold || 50
             const wordsToSkip = Number(body.wordsToSkip || 0)
+            const url = `${req.headers.host}${req.url}`
+            const logRequest = options => {
+                const url = options?.url
+                const httpMethod = options?.httpMethod
+                const statusCode = options?.statusCode
+                const error = options?.error
+                const reqBody = options?.reqBody
+        
+                console.log(`
+                HTTP ${httpMethod} ${url}
+                statusCode = ${statusCode}
+                ${error ? `error = ${JSON.stringify(error)}
+                reqBody = ${JSON.stringify(reqBody)}` : ''}
+                `)
+            }
 
             if (wordsToSkip < 0) {
-                res.writeHead(404, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({
-                    data: {
-                        message: 'Error!',
-                        text: 'wordsToSkip can not be minor then 0'
-                    }
-                }))
+                const statusCode = 404
+                const errorBody = {
+                    message: 'Error!',
+                    text: 'wordsToSkip can not be minor then 0'
+                }
                 
+                res.writeHead(statusCode, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ data: errorBody }))
+
+                logRequest({
+                    url,
+                    httpMethod,
+                    statusCode,
+                    error: errorBody,
+                    reqBody: body
+                })
+
                 return
             }
 
@@ -30,14 +56,21 @@ http.createServer((req, res) => {
                 fileType,
             }
             const boldedText = processText(processOptions)
+            const responseBody = {
+                message: 'Bolded text successfully!',
+                text: boldedText
+            }
+            const statusCode = 200
+            
+            logRequest({
+                url,
+                httpMethod,
+                statusCode,
+                reqBody: body
+            })
 
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({
-                data: {
-                    message: 'Bolded text successfully!',
-                    text: boldedText
-                }
-            }))
+            res.writeHead(statusCode, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ data: responseBody }))
         })
     } else if (req.method === 'GET') {
         res.writeHead(200, { 'content-Type': 'application/json' })
