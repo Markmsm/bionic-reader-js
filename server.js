e10fb4c2b861e259a43b7dcd6627da40a824de61
Mark <marksmunari@gmail.com>
2024-02-07 07:45:59 -0300

    add try catch in server

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ import * as http from 'http'
 import processText from './core.js'
 
 
+const logRequest = options => {
+    const url = options?.url
+    const httpMethod = options?.httpMethod
+    const statusCode = options?.statusCode
+    const error = options?.error
+    const reqBody = options?.reqBody
+
+    console.log(`
+    HTTP ${httpMethod} ${url}
+    statusCode = ${statusCode}
+    ${error ? `error = ${JSON.stringify(error)}
+    reqBody = ${JSON.stringify(reqBody)}` : ''}
+    `)
+}
+
 http.createServer((req, res) => {
     const httpMethod = req.method
 
@@ -13,28 +28,38 @@ http.createServer((req, res) => {
             const percentageOfWordToBold = body.percentageOfWordToBold || 50
             const wordsToSkip = Number(body.wordsToSkip || 0)
             const url = `${req.headers.host}${req.url}`
-            const logRequest = options => {
-                const url = options?.url
-                const httpMethod = options?.httpMethod
-                const statusCode = options?.statusCode
-                const error = options?.error
-                const reqBody = options?.reqBody
-        
-                console.log(`
-                HTTP ${httpMethod} ${url}
-                statusCode = ${statusCode}
-                ${error ? `error = ${JSON.stringify(error)}
-                reqBody = ${JSON.stringify(reqBody)}` : ''}
-                `)
-            }
 
-            if (wordsToSkip < 0) {
+            try {
+                const processOptions = {
+                    textToProcess,
+                    percentageOfWordToBold,
+                    wordsToSkip,
+                    fileType,
+                }
+                const responseBody = {
+                    message: 'Bolded text successfully!',
+                    text: processText(processOptions)
+                }
+                const statusCode = 200
+
+                logRequest({
+                    url,
+                    httpMethod,
+                    statusCode,
+                    reqBody: body
+                })
+
+                res.writeHead(statusCode, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ data: responseBody }))
+            } catch (e) {
+                console.log(e)
+
                 const statusCode = 404
                 const errorBody = {
                     message: 'Error!',
-                    text: 'wordsToSkip can not be minor then 0'
+                    text: e.message
                 }
-                
+
                 res.writeHead(statusCode, { 'Content-Type': 'application/json' })
                 res.end(JSON.stringify({ data: errorBody }))
 
@@ -45,32 +70,7 @@ http.createServer((req, res) => {
                     error: errorBody,
                     reqBody: body
                 })
-
-                return
             }
-
-            const processOptions = {
-                textToProcess,
-                percentageOfWordToBold,
-                wordsToSkip,
-                fileType,
-            }
-            const boldedText = processText(processOptions)
-            const responseBody = {
-                message: 'Bolded text successfully!',
-                text: boldedText
-            }
-            const statusCode = 200
-            
-            logRequest({
-                url,
-                httpMethod,
-                statusCode,
-                reqBody: body
-            })
-
-            res.writeHead(statusCode, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ data: responseBody }))
         })
     } else if (req.method === 'GET') {
         res.writeHead(200, { 'content-Type': 'application/json' })
