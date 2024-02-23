1e9d724969bf2af20db131fecf79b6f1dbe10602
Mark <marksmunari@gmail.com>
2024-02-23 11:48:08 -0300

    add more unit tests

--- test/core.test.js
+++ test/core.test.js
@@ -40,13 +40,97 @@ describe('processText', () => {
         assert.deepStrictEqual(result, expecetResult)
     })
 
+    it('should return text with each half word bolded', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: 'boldWord'
+        }
+        const expecetResult = '\x1b[1mbold\x1b[0mWord'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
+    it('should return bolded text regarding e-mail as word', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: 'email.teste@emailteste.com'
+        }
+        const expecetResult = '\x1b[1memail.teste@e\x1b[0mmailteste.com'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
+    it('should return bolde text with penultimate punctuation when has two final ponctuaction', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: 'boldWord.:'
+        }
+        const expecetResult = '\x1b[1mboldW\x1b[0mord.:'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
+    it('should return bolded text disregarding ellipses at init of phrase', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: '...boldWord teste'
+        }
+        const expecetResult = '...\x1b[1mbold\x1b[0mWord \x1b[1mtes\x1b[0mte'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
+    it('should return bolded text disregarding ellipses at end of phrase', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: 'boldWord teste...'
+        }
+        const expecetResult = '\x1b[1mbold\x1b[0mWord \x1b[1mtes\x1b[0mte...'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
+    it('should return bolded text disregarding ellipses at init and end of word', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: '...dos...'
+        }
+        const expecetResult = '...\x1b[1mdo\x1b[0ms...'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
     it('', () => {
         // Given:
 
         // When:
 
         // Then:
-        
+
     })
 
     
