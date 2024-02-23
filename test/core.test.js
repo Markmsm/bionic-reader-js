d5e961a52afeea5725a13aa7d95e7f320777adda
Mark <marksmunari@gmail.com>
2024-02-23 11:25:58 -0300

    git add test for core

--- test/core.test.js
+++ test/core.test.js
@@ -25,4 +25,29 @@ describe('processText', () => {
         assert.throws(() => processText(processOptions),
             { message: `wordsToSkip can't be minor than 0` })
     })
+
+    it('should return bolded text properly', () => {
+        // Given:
+        const processOptions = {
+            textToProcess: 'texto qualquer para teste'
+        }
+        const expecetResult = '\x1b[1mtex\x1b[0mto \x1b[1mqual\x1b[0mquer \x1b[1mpa\x1b[0mra \x1b[1mtes\x1b[0mte'
+
+        // When:
+        const result = processText(processOptions)
+
+        // Then:
+        assert.deepStrictEqual(result, expecetResult)
+    })
+
+    it('', () => {
+        // Given:
+
+        // When:
+
+        // Then:
+        
+    })
+
+    
 })
